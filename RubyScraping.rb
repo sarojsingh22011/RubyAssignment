bd7fca5d87859bf25444310422f26eeaac472c7b
sarojwebonise <saroj@weboniselab.com>
2012-12-17 17:49:51 +0530

    data fetched and stored in database successfullygit add .

--- RubyScraping.rb
+++ RubyScraping.rb
@@ -4,9 +4,6 @@ require 'nokogiri'
 require 'active_record'
 #require 'mysql2'
 
-
-$arr1 = Array.new
-$arr2 = Array.new
 ActiveRecord::Base.establish_connection(
 :adapter => "mysql2",
 :database => "recipes",
@@ -18,25 +15,8 @@ ActiveRecord::Base.establish_connection(
 class Recipetable < ActiveRecord::Base
 end
 doc = Nokogiri::HTML(open('http://www.simplyrecipes.com/index/'))
-
-doc.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "entry-content", " " ))]//p/a/@href').each_with_index do |node|
-   $arr2 << node.text
- end
-
-doc.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "entry-content", " " ))]//p/a').each_with_index do |node|
-   $arr1 << node.text
+doc.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "entry-content", " " ))]//p/a').each do |node|
+    Recipetable.create(:rname => node.content,:url => node['href'])
+    link=node['href']
    
- end
-
-$arr1.each do |a1|
-puts a1
-end
-
-$arr2.each do |a2|
-puts a2
-end
-
-$arr1.each do |index|
-Recipetable.create(:rname => index);
-
 end
