03a1cd8bc8477d1fd70595257df8acf04a071c5a
sarojwebonise <saroj@weboniselab.com>
2012-12-17 16:03:48 +0530

    coding done.

--- RubyScraping.rb
+++ RubyScraping.rb
@@ -2,9 +2,21 @@ require 'rubygems'
 require 'open-uri'
 require 'nokogiri'
 require 'active_record'
+#require 'mysql2'
+
 
 $arr1 = Array.new
 $arr2 = Array.new
+ActiveRecord::Base.establish_connection(
+:adapter => "mysql2",
+:database => "recipes",
+:username => "root",
+:password => "saroj",
+:host => "127.0.0.1"
+)
+
+class Recipetable < ActiveRecord::Base
+end
 doc = Nokogiri::HTML(open('http://www.simplyrecipes.com/index/'))
 
 doc.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "entry-content", " " ))]//p/a/@href').each_with_index do |node|
@@ -13,29 +25,18 @@ doc.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "entry-content"
 
 doc.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "entry-content", " " ))]//p/a').each_with_index do |node|
    $arr1 << node.text
+   
  end
 
-#$arr1.each do |a1|
-#puts a1
-#end
-
-#$arr2.each do |a2|
-#puts a2
-#end
-
-ActiveRecord::Base.establish_connection(
-:adapter=>"mysql2",
-:host=>"localhost",
-:database=>"recipes",
-:username=>"saroj",
-:password=>"saroj"
-)
-
-class recipetable < ActiveRecord::Base
+$arr1.each do |a1|
+puts a1
 end
-$i=0
-while $i<500 do
-$val=$a1.at($i)
-recipetable.create(:id => #$i, :recipe_list => "#$val")
-$i +=1
+
+$arr2.each do |a2|
+puts a2
+end
+
+$arr1.each do |index|
+Recipetable.create(:rname => index);
+
 end
