26386828b0c57926c8bad802c09c0cdc9a0abc2a
sarojwebonise <saroj@weboniselab.com>
2012-12-18 18:45:37 +0530

    coding done.

--- RubyScraping.rb
+++ RubyScraping.rb
@@ -9,33 +9,31 @@ ActiveRecord::Base.establish_connection(
 :database => "recipes",
 :username => "root",
 :password => "saroj",
-:host => "127.0.0.1"
+:host => "localhost"
 )
 
 class Recipetable < ActiveRecord::Base
 end
 doc = Nokogiri::HTML(open('http://www.simplyrecipes.com/index/'))
 doc.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "entry-content", " " ))]//p/a').each do |node|
-    Recipetable.create(:rname => node.content,:url => node['href'])
-    link=node['href']
-   
-   doc1=Nokogiri::HTML(open(link))
-    doc1.xpath('//h2/a').each do |node|
-    Recipeindex.create(:iname => node.content,:url1 => node['href'])
-    link2=node['href']
-    =begin  
-doc2=Nokogiri::HTML(open(link2))
-   x=doc2.xpath('//div[@itemprop="description"]')
-   e.gsub!(/(<[^>]*>)|\n|\t/s){""}
-   
-   ingredient=doc2.xpath('//div[@id="recipe-ingredients"]/ul/li[@class="ingredient"]').to_s() #.gsub!(/(<[^>]*>)|\n|\t/s){""}
-   ingredient.gsub!(/(<[^>]*>)|\n|\t/s){""}
+link=node['href']
 
-   method=doc2.xpath('//div[@id="recipe-method"]/div[@itemprop="recipeInstructions"]').to_s()
-   method.gsub!(/(<[^>]*>)|\n|\t/s){""}
-    =end
+   
+doc1=Nokogiri::HTML(open(link))
+doc1.xpath('//h2/a').each do |node|
+link2=node['href']
     
+doc2 = Nokogiri::HTML(open(link2))
+x=doc2.xpath('//div[@itemprop="description"]')
+puts x
+image_link=doc2.xpath('//div[@class="featured-image"]/img/@src')
+puts image_link
+ingredient=doc2.xpath('//div[@id="recipe-ingredients"]/ul/li[@class="ingredient"]').to_s() 
+puts ingredient
+instr=doc2.xpath('//div[@id="recipe-method"]/div[@itemprop="recipeInstructions"]').to_s()
+puts instr				
+Recipetable.create(:rname => node.content,:url => link,:rdes => x,:imglnk => image_link,:ringred => ingredient,:rinst => instr)
+			 
 
-  end  
-    
+end
 end
